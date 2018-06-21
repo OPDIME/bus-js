909f355b97e0d481562f4fccc00dbea011d310ad
Jörn Neumeyer <joernneumeyer@gmail.com>
2018-06-21 17:00:39 +0200

    adjusted style

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "roots": [
-    "<rootDir>/src"
+    "./src"
   ],
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
@@ -14,4 +14,4 @@ module.exports = {
     "json",
     "node"
   ],
-}
+};
