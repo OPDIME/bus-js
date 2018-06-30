f1f384032459a76211c23a1be5c93d5aa32cd4dc
Jörn Neumeyer <joernneumeyer@gmail.com>
2018-06-30 20:33:47 +0200

    refactored; added JSDoc

--- src/bus-manager.ts
+++ src/bus-manager.ts
@@ -8,6 +8,10 @@ export class BusManager {
   public static readonly ALL_CHANNEL = '*';
   private static busses: { [k: string]: (Bus | undefined) } = { };
 
+  /**
+   * Register a bus for a channel.
+   * This may overwrite an existing channel bus!
+   */
   private static registerBus(channel: string): Bus {
     const bus = new Bus(channel);
     BusManager.busses[channel] = bus;
@@ -15,10 +19,18 @@ export class BusManager {
     return bus;
   }
 
+  /**
+   * Returns the bus which belongs to the given channel.
+   * May return undefined if the channel has no bus!
+   */
   public static getChannelBus(channel: string): (Bus | undefined) {
     return BusManager.busses[channel];
   }
 
+  /**
+   * Returns the bus which belongs to the given channel.
+   * May create the channel bus if it does not exist.
+   */
   public static getChannelBusOrCreate(channel: string): Bus {
     const bus = BusManager.busses[channel];
     if (bus === undefined) {
@@ -28,14 +40,26 @@ export class BusManager {
     return bus;
   }
 
+  /**
+   * Returns the bus which belongs to the given channel.
+   * May create the channel bus if it does not exist.
+   *
+   * Alias for getChannelBusOrCreate.
+   */
   public static channel(channel: string): Bus {
     return BusManager.getChannelBusOrCreate(channel);
   }
 
+  /**
+   * Returns the all channel.
+   */
   public static channelAll(): Bus {
     return BusManager.channel(BusManager.ALL_CHANNEL);
   }
 
+  /**
+   * Subscribe to the global channel or a specified channel.
+   */
   public static subscribe(
     subscriber: BusSubscriber,
     channel: string = BusManager.ALL_CHANNEL
@@ -46,6 +70,9 @@ export class BusManager {
     }
   }
 
+  /**
+   * Unsubscribe to the global channel or a specified channel.
+   */
   public static unsubscribe(
     subscriber: BusSubscriber,
     channel: string = BusManager.ALL_CHANNEL
@@ -56,6 +83,9 @@ export class BusManager {
     }
   }
 
+  /**
+   * Publish a message to the global bus and the specified bus.
+   */
   public static publish(message: IBusMessage) {
     if (message.channel) {
       BusManager
@@ -63,7 +93,7 @@ export class BusManager {
         .publish(message);
     }
     BusManager
-      .channel(BusManager.ALL_CHANNEL)
+      .channelAll()
       .publish(message);
   }
 }
